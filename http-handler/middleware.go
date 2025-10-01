909c1f3896f9a670a2979601b166def49536e4fc
Fabien Burguion <fabien.burguion@epita.fr>
2025-10-01 18:11:31 +0200

    feat: rate limiter less punitive

--- http-handler/middleware.go
+++ http-handler/middleware.go
@@ -44,7 +44,7 @@ func getVisitor(ip string) *rate.Limiter {
 
 	limiter, exists := visitors[ip]
 	if !exists {
-		limiter = rate.NewLimiter(1, 3)
+		limiter = rate.NewLimiter(5, 10)
 		visitors[ip] = limiter
 	}
 	return limiter
