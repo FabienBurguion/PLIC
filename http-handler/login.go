791bca9546750f7910ceed4b58afda2e5d2fc758
fabien.burguion <fabien.burguion@epita.fr>
2025-12-01 16:50:50 +0100

    debug: password reset bug

--- http-handler/login.go
+++ http-handler/login.go
@@ -332,6 +332,8 @@ func (s *Service) ResetPassword(w http.ResponseWriter, r *http.Request, _ models
 		return httpx.WriteError(w, http.StatusInternalServerError, "Could not generate password")
 	}
 
+	logger.Info().Msg("New password generated: " + newPassword)
+
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
 	if err != nil {
 		logger.Error().Err(err).Msg("password hashing failed")
